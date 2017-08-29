9861c9a65ba3e35f2457be29a9def057e7467b42
haseebOptini <ahaseeb91@gmail.com>
2017-08-29 19:14:51 +0500

    Code refactoring.

--- Source/MCQAPI.swift
+++ Source/MCQAPI.swift
@@ -10,51 +10,6 @@ import Foundation
 import edXCore
 import SwiftyJSON
 
-class MCQResponseData: NSObject {
-    struct Keys {
-        static let id = "id"
-        static let value = "submission"
-        static let status = "completed"
-        static let tip = "tips"
-    }
-
-    let id: String
-    let value: String
-    let status: Bool
-    let tip: String
-    
-    init(id: String, value: String, status: Bool, tip: String) {
-        self.id = id
-        self.value = value
-        self.status = status
-        self.tip = tip
-    }
-    init?(dictionary: [String: Any]) {
-        self.id = dictionary[Keys.id] as? String ?? ""
-        self.value = dictionary[Keys.value] as? String ?? ""
-        self.status = dictionary[Keys.status] as? Bool ?? false
-        self.tip = dictionary[Keys.tip] as? String ?? ""
-        
-        super.init()
-    }
-    
-    convenience init?(json: JSON) {
-        let array = json.arrayValue
-        if array.count >= 2 {
-            let id = array[0].stringValue
-            guard var dictionary = json.dictionaryObject else {
-                self.init(dictionary:[:])
-                return nil
-            }
-            dictionary["id"] = id
-            self.init(dictionary: dictionary)
-        } else {
-            self.init(dictionary:[:])
-            return nil
-        }
-    }
-}
-
 struct MCQAPI {
     struct Keys {
         static let completed = "completed"
@@ -63,7 +18,7 @@ struct MCQAPI {
         static let results = "results"
     }
 
-    static func mcqResponseDeserializer(_ response: HTTPURLResponse, json: JSON) -> Result<MCQResponseData> {
+    static func mcqResponseDeserializer(_ response: HTTPURLResponse, json: JSON) -> Result<Bool> {
 
         //TODO: Need to implement the mapping of response to MCQResponseData and send the response for MCQResponse
        guard let mcqResponse = json.dictionary else {
@@ -71,24 +26,11 @@ struct MCQAPI {
         }
         
         let isCompleted = mcqResponse[Keys.completed]?.boolValue ?? false
-        var id: String = ""
-        var value: String = ""
-        var tip: String = ""
         
-        let results = mcqResponse[Keys.results]?.arrayValue
-        if (results?.count ?? 0) >= 2 {
-            id = (results?[0].stringValue) ?? ""
-            if let optionResult = results?[1].dictionaryValue {
-                value = (optionResult[Keys.value]?.stringValue) ?? ""
-                tip = (optionResult[Keys.value]?.stringValue) ?? ""
-            }
-        }
-        
-        let mcqResponseData = MCQResponseData(id: id, value: value, status: isCompleted, tip: tip)
-        return .success(mcqResponseData)
+        return .success(isCompleted)
     }
     
-    static func getMCQResponse(_ questionId: String, value: String, courseId: String, blockId: String) -> NetworkRequest<MCQResponseData> {
+    static func getMCQResponse(_ questionId: String, value: String, courseId: String, blockId: String) -> NetworkRequest<Bool> {
         let path = "/courses/{course_id}/xblock/{block_id}".oex_format(withParameters: ["course_id": courseId, "block_id": blockId])
         let requestBody = [questionId: ["value": value]]
         return NetworkRequest(method: .GET,
--- Source/MCQManager.swift
+++ Source/MCQManager.swift
@@ -14,7 +14,7 @@ class MCQManager: NSObject, MCQResultMatching {
     let blockID: String
     let courseID: String
     let enviroment: RouterEnvironment
-    var stream: edXCore.Stream<MCQResponseData>?
+    var stream: edXCore.Stream<Bool>?
 
     public init(blockID: String, courseID: String, environment: RouterEnvironment) {
         self.blockID = blockID
@@ -24,18 +24,15 @@ class MCQManager: NSObject, MCQResultMatching {
     
     public func matchMCQ(questionId: String, value: String, completion: @escaping (Bool) -> Swift.Void) {
         // TODO: call the method to get the stream on the basis of questionID and value of the option
-        self.stream = mcqResponseStream(questionId: questionId, value: value, courseId: self.courseID, blockId: self.blockID)
+//        self.stream = mcqResponseStream(questionId: questionId, value: value, courseId: self.courseID, blockId: self.blockID)
 //        self.stream?.listen(self, action: { (result) in
-//            
-//        result.ifSuccess({ (mcqResponseData: MCQResponseData) -> Void in
-//                completion(mcqResponseData.status)
-//            
+//        result.ifSuccess({ (success: Bool) -> Void in
+//                completion(success)
 //            })
 //        
 //            result.ifFailure({ (error) in
 //                print(error.localizedDescription)
 //            })
-//        
 //        })
         completion(false)
     }
@@ -43,7 +40,7 @@ class MCQManager: NSObject, MCQResultMatching {
 
 extension MCQManager {
 
-    func mcqResponseStream(questionId: String, value: String, courseId: String, blockId: String) -> edXCore.Stream<MCQResponseData> {
+    func mcqResponseStream(questionId: String, value: String, courseId: String, blockId: String) -> edXCore.Stream<Bool> {
         let request = MCQAPI.getMCQResponse(questionId, value: value, courseId: courseId, blockId: blockId)
         return enviroment.networkManager.streamForRequest(request)
     }
