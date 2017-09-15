d4df73cfb12dd3787b0ae39c32022fb9409515aa
Salman Jamil <salmanjamil@SalmanJamil-MBP-107.arbisoft.com>
2017-09-15 14:49:13 +0500

    Added Styles file to apply CSS to HTML in textVIew

---
+++ Source/HTMLStyles.swift
@@ -0,0 +1,42 @@
+//
+//  HTMLStyles.swift
+//  edX
+//
+//  Created by Salman Jamil on 9/11/17.
+//  Copyright © 2017 edX. All rights reserved.
+//
+
+import Foundation
+
+let questionTemplate: (String) -> String = { body in
+    return "<html> <head> <style type=\"text/css\">  body {  font-size: 14px;  font-family: -apple-system, Arial, sans-serif;  color: white; margin-bottom: 0px; padding-bottom: 0px; line-height: 20px; } </style> </head> <body> \(body) </body> </html>"
+}
+
+extension String {
+    func styled(with template: (String) -> String) -> String {
+        return template(self)
+    }
+}
+
+extension NSAttributedString {
+    convenience public init?(styled: String) {
+        guard let data = styled.data(using: .unicode) else {
+            return nil
+        }
+        do {
+            let string = try NSMutableAttributedString(data: data,
+                                                       options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType], documentAttributes: nil)
+            self.init(attributedString: string.removingTrailingNewLine)
+        } catch {
+            return nil
+        }
+    }
+    
+    var removingTrailingNewLine: NSAttributedString {
+        if string.hasSuffix("\n") {
+            return attributedSubstring(from: NSRange(location:0, length: length - 1))
+        }
+        return self
+    }
+}
+
