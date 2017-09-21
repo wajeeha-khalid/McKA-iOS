3ba7f592ee1534de0ace252e7b69a4c532e7df68
mtbtalha <talha.babar@arbisoft.com.pk>
2017-09-21 15:46:21 +0500

    Poll Implementation

--- Source/CourseOutline.swift
+++ Source/CourseOutline.swift
@@ -129,10 +129,6 @@ public struct CourseOutline {
                         let studentViewData = body[Fields.StudentViewData]
                         let content = studentViewData[Fields.html].stringValue
                         type = .html(content)
-                    case CourseBlock.Category.IMAGEEXPLORER:
-                        let studentViewData = body[Fields.StudentViewData]
-                        let content = studentViewData[Fields.html].stringValue
-                        type = .html(content)
                     case CourseBlock.Category.Problem:
                         type = .problem
                     case .OOYALA:
@@ -259,7 +255,6 @@ open class CourseBlock {
         case Chapter = "chapter"
         case Course = "course"
         case HTML = "html"
-        case IMAGEEXPLORER = "image-explorer"
         case OOYALA = "ooyala-player"
         case Problem = "problem"
         case Section = "sequential"
