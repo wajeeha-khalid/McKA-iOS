a47ca466c7b34231aa95e1f3da5008f753fb013d
shafqat-muneer <shafqat.muneer@arbisoft.com>
2017-09-25 19:44:46 +0500

    Pod file updates.

---
+++ Pods/Headers/Private/UrbanAirship-iOS-SDK/UASwizzler+Internal.h
@@ -0,0 +1,1 @@
+../../../UrbanAirship-iOS-SDK/AirshipKit/AirshipKit/UASwizzler+Internal.h
--- Source/EnrollmentManager.swift
+++ Source/EnrollmentManager.swift
@@ -98,25 +98,22 @@ open class EnrollmentManager : NSObject {
     }
     
     func freshFeedWithUsername(_ username: String, organizationCode: String?) -> Feed<[(UserCourseEnrollment,ProgressStats)]> {
-        
-        
-        
         let request = CoursesAPI.getUserEnrollments(username, organizationCode: organizationCode)
         let enrollmentStream = networkManager.streamForRequest(request, persistResponse: true)
         let progressStream = networkManager.streamForRequest(CourseProgressAPI.getAllCoursesProgress(), persistResponse: true)
         
-        
         let combinedStream: edXCore.Stream<[(UserCourseEnrollment, ProgressStats)]> = joinStreams(enrollmentStream, progressStream).map { (enrollements, progress) in
-            
             //Done because of in-consistent number of elements returned in CourseEnrollments & CourseProgress from API
-            return enrollements.flatMap { enrollment -> (UserCourseEnrollment, ProgressStats)? in
+            return enrollements.map { enrollment -> (UserCourseEnrollment, ProgressStats) in
                 guard let courseProgress = progress.index(where: {
                     $0.courseID == enrollment.course.course_id
                 })
                 .map ({
                     progress[$0]
                 }) else {
-                        return nil
+                        return (
+                            enrollment, ProgressStats(courseID: enrollment.course.course_id!, earned: 0, possible: 0, ratio: 0.0)
+                        )
                 }
                 return (enrollment, courseProgress)
             }
